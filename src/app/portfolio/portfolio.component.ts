285676a9ec214efe5d8ea2eb8f9b46abb4295031
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-09-07 14:51:48 +0300

    portfolio component

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -8,4 +8,32 @@ import { Component } from '@angular/core';
 export class PortfolioComponent {
   color: string = '#2c3e50';
   title: string = 'Portfolio';
+  selectedProjectIndex: number = 0;
+
+  projects: { id: number; imgSrc: string }[] = [
+    {
+      id: 1,
+      imgSrc: 'assets/poert1.png',
+    },
+    {
+      id: 2,
+      imgSrc: 'assets/port2.png',
+    },
+    {
+      id: 3,
+      imgSrc: 'assets/port3.png',
+    },
+    {
+      id: 4,
+      imgSrc: 'assets/poert1.png',
+    },
+    {
+      id: 5,
+      imgSrc: 'assets/port2.png',
+    },
+    {
+      id: 6,
+      imgSrc: 'assets/port3.png',
+    },
+  ];
 }
